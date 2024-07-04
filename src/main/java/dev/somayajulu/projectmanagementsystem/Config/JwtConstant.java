33715d133f82f626475b374bc6bd084108eaf50a
SomayajuluSharma <chsomayajulusharma@gmail.com>
2024-07-04 17:37:45 +0530

    Add signup and signin APIs for user authentication

---
+++ src/main/java/dev/somayajulu/projectmanagementsystem/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package dev.somayajulu.projectmanagementsystem.Config;
+
+public class JwtConstant {
+    public static final String SECRETE_KEY = "stunningisaheroyouknowiknowaahejwtjkrhewthwjhhertjwfjewbfnwejfjwehfjewbkjewnvjwnjrjewhrjwehjkfbwkjvkjwejkrwjehefjewvjlkpqowpoeeqewpojfwnvoiweijrweihfkbwjvbwoqkefoiweqroirqjekwqnvowne";
+    public static final String JWT_HEADER = "Authorization";
+}
